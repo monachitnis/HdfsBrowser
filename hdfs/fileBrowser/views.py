15f1c68ba93be0786d2b63037fda2e229ac2e6cf
Mona Chitnis <chitnis@crosslotoil-lm.corp.yahoo.com>
2013-05-30 16:33:58 -0700

    cleaned up views.py

--- hdfs/fileBrowser/views.py
+++ hdfs/fileBrowser/views.py
@@ -1,21 +1,17 @@
+#!/usr/bin/env python
 from django.views.decorators.csrf import csrf_protect
 from django.http import HttpResponse
 import pexpect
 import os, sys, time, re, getopt, getpass
 import traceback
+import httplib, subprocess
 from django.views.decorators.csrf import csrf_exempt
 from django.core.context_processors import csrf
 from django.shortcuts import render_to_response
 from django import template 
 from django.template import Template, Context, RequestContext, loader
 
-SSH_NEWKEY = '(?i)are you sure you want to continue connecting'
-
 COMMAND_PROMPT = '[#$] '
-user='siddharn'
-host='mithril-gw.red.ygrid.yahoo.com'
-password='281Laxman!'
-
 
 @csrf_protect
 def login_user(request):
@@ -39,80 +35,47 @@ def get_grid_list():
     grid_list = ['files.py', 'abc.txt']
     return grid_list
 
-
 def grid_listing(request):
 	return render_to_response('fileBrowser/gridListing.html')
-
-def list_view(request):
-	child = pexpect.spawn('ssh -l %s %s'%(user, host))
-	COMMAND_PROMPT = '[#$] '
-	TERMINAL_PROMPT = '(?i)terminal type\?'
-	TERMINAL_TYPE = 'vt100'
-    	i = child.expect([pexpect.TIMEOUT, SSH_NEWKEY, COMMAND_PROMPT, '(?i)password'])
-    	if i == 0: # Timeout
-        	print 'ERROR! could not login with SSH. Here is what SSH said:'
-        	print child.before, child.after
-        	print str(child)
-        	sys.exit (1)
-    	if i == 1: # In this case SSH does not have the public key cached.
-        	child.sendline ('yes')
-        	child.expect ('(?i)password')
-    	if i == 2:
-        # This may happen if a public key was setup to automatically login.
-        # But beware, the COMMAND_PROMPT at this point is very trivial and
-        # could be fooled by some output in the MOTD or login message.
-        	pass
-    	if i == 3:
-        	child.sendline(password)
-        # Now we are either at the command prompt or
-        # the login process is asking for our terminal type.
-       	 	i = child.expect ([COMMAND_PROMPT, TERMINAL_PROMPT])
-        	if i == 1:
-            		child.sendline (TERMINAL_TYPE)
-            		child.expect (COMMAND_PROMPT)
-    #
-    # Set command prompt to something more unique.
-    #
-    	COMMAND_PROMPT = "\[PEXPECT\]\$ "
-    	child.sendline ("PS1='[PEXPECT]\$ '") # In case of sh-style
-    	i = child.expect ([pexpect.TIMEOUT, COMMAND_PROMPT], timeout=10)
-    	if i == 0:
-        	print "# Couldn't set sh-style prompt -- trying csh-style."
-        	child.sendline ("set prompt='[PEXPECT]\$ '")
-        	i = child.expect ([pexpect.TIMEOUT, COMMAND_PROMPT], timeout=10)
-        	if i == 0:
-            		print "Failed to set command prompt using sh or csh style."
-            		print "Response was:"
-            		print child.before
-            		sys.exit (1)
+ 
+def hdfs_proxy(request):
 	
-	#Start sending commands
-#	child.sendline ('ls -al')
-#    	child.expect (COMMAND_PROMPT)
-#    	ls = child.before
-
-   	child.sendline ('kinit')
-    	child.expect ('Password for siddharn@Y.CORP.YAHOO.COM: ')
-   	child.sendline ('281Laxman!')
-    	child.expect (COMMAND_PROMPT)
-    	kinit = child.before
-
-    # Run hadoop fs -ls.
-    	child.sendline ('hadoop fs -ls')
-    	child.expect (COMMAND_PROMPT)
-    	hls = child.before
+	username='chitnis'
+	realm='DS.CORP.YAHOO.COM'
+	principal = username + '@' + realm
+	password='Eelix!r01'
+   	child = pexpect.spawn ('kinit', [principal])
+    	child.expect ('Password for ' + principal + ': ')
+   	child.sendline (password)
+    	#child.expect (COMMAND_PROMPT) # EOF exception!!
 	
-    	child.sendline ('hadoop fs -put config.yaml .')
-    	child.expect (COMMAND_PROMPT)
-    	hput = child.before
+	curl = '/usr/bin/curl'
+	cert = '/home/y/conf/ygrid_cacert/ca-cert.pem'
+        proxy_server = 'https://axoniteblue-nn1-pxy.blue.ygrid.yahoo.com:4443/fs/'
+	operation = 'status'
+	ws_url = proxy_server + 'user/' + username + '?op=' + operation
+	curl_args = [ curl, '--cacert', cert, '--negotiate', '-u', 'login:key', ws_url ]
+	curl = subprocess.Popen(curl_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	#curl = subprocess.Popen(curl_args, shell=True)
+	out, err = curl.communicate()
+	#curl.terminate();
+	print(curl)
+	return HttpResponse("<html> <body><li>"+out+"</li> </body></html>" )
 	
-	return HttpResponse("<html> <body> Hello, World: \n <li>"+hls+"</li> </body></html>" )
+        #curl = pycurl.Curl()
+	#curl.setopt(curl.VERBOSE, 1)
+	#curl.setopt(pycurl.URL, proxy)
+        #curl.setopt(pycurl.CAINFO, certdir)
+	#curl.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_GSSNEGOTIATE)
+	#curl.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_BASIC)
+	#curl.perform()
+	#curl.close()
 
 def browser_view(request):
 	template = loader.get_template('fileBrowser/browse.html')
 	context = Context({})
         return HttpResponse(template.render(context))
-		
+
 @csrf_exempt		
 def api_view(request):
 	r=['	<ul class="jqueryFileTree" style="display: none;">\
