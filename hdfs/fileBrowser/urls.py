6cf20ed6b2e857bf23c73b8425fa1d42f6f0d630
anupamam <anupamam@yahoo-inc.com>
2013-05-31 02:10:42 -0700

    New UI YAYYYY

--- hdfs/fileBrowser/urls.py
+++ hdfs/fileBrowser/urls.py
@@ -4,6 +4,8 @@ from fileBrowser import views
 
 urlpatterns = patterns('',
     url(r'^browser$', views.browser_view, name='browser_view'),
+    url(r'^info$', views.dir_info, name='dir_info'),
+    url(r'^gridListing$', views.grid_listing, name='grid_listing'),
     url(r'^api$', views.api_view, name='api_view'),
     url(r'^hdfsBrowser$', views.first_load, name='first_load'),
     url(r'^list$', views.list, name='list'),
