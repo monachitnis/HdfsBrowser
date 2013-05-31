63c823b60dcee9c7d8f3d66ed62907f625f83018
Mona Chitnis <chitnis@crosslotoil-lm.corp.yahoo.com>
2013-05-30 20:29:18 -0700

    function mappings

--- hdfs/fileBrowser/urls.py
+++ hdfs/fileBrowser/urls.py
@@ -3,10 +3,14 @@ from django.conf.urls import patterns, url
 from fileBrowser import views
 
 urlpatterns = patterns('',
-    #url(r'^$', views.list_view, name='list_view')
-    #url(r'^$', views.browser_view, name='browser_view')
-	url(r'^login/$', views.login_user, name='login_user'),
-	url(r'^gridListing$', views.grid_listing, name='grid_listing'),
-	url(r'^browser$', views.browser_view, name='browser_view'),
-	url(r'^api$', views.api_view, name='api_view')
+    url(r'^browser$', views.browser_view, name='browser_view'),
+    url(r'^api$', views.api_view, name='api_view'),
+    url(r'^hdfsBrowser$', views.first_load, name='first_load'),
+    url(r'^list$', views.list, name='list'),
+    url(r'^mkdir$', views.mkdir, name='mkdir'),
+    url(r'^delete$', views.delete, name='delete'),
+    url(r'^upload$', views.upload, name='upload'),
+    url(r'^move$', views.move, name='move'),
+    url(r'^chown$', views.chown, name='chown'),
+    url(r'^chmod$', views.chmod, name='chmod')
 )
--- hdfs/fileBrowser/views.py
+++ hdfs/fileBrowser/views.py
@@ -12,6 +12,9 @@ from django import template
 from django.template import Template, Context, RequestContext, loader
 
 COMMAND_PROMPT = '[#$] '
+curl = '/usr/bin/curl'
+cert = '/home/y/conf/ygrid_cacert/ca-cert.pem'
+username = ''
 
 @csrf_protect
 def login_user(request):
@@ -38,56 +41,85 @@ def get_grid_list():
 def grid_listing(request):
 	return render_to_response('fileBrowser/gridListing.html')
  
-def hdfs_proxy(request):
-	
-	username='chitnis'
-	realm='DS.CORP.YAHOO.COM'
-	principal = username + '@' + realm
-	password='Eelix!r01'
-   	child = pexpect.spawn ('kinit', [principal])
-    	child.expect ('Password for ' + principal + ': ')
-   	child.sendline (password)
-	COMMAND_PROMPT = "\[PEXPECT\]\$ "
+def first_load(request):
+        username='chitnis'
+        realm='DS.CORP.YAHOO.COM'
+        principal = username + '@' + realm
+        password='Eelix!r01' 
+        child = pexpect.spawn ('kinit', [principal])
+        child.expect ('Password for ' + principal + ': ')
+        child.sendline (password)
+        COMMAND_PROMPT = "\[PEXPECT\]\$ "
         child.sendline ("PS1='[PEXPECT]\$ '")
-    	#child.expect (COMMAND_PROMPT) # EOF exception!!
-	
-	curl = '/usr/bin/curl'
-	cert = '/home/y/conf/ygrid_cacert/ca-cert.pem'
+        #child.expect (COMMAND_PROMPT) # EOF exception!!
+        #curl = pycurl.Curl()
+        #curl.setopt(curl.VERBOSE, 1)
+        #curl.setopt(pycurl.URL, proxy)
+        #curl.setopt(pycurl.CAINFO, certdir)
+        #curl.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_GSSNEGOTIATE)
+        #curl.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_BASIC)
+        #curl.perform()
+        #curl.close()
         proxy_server = 'https://axoniteblue-nn1-pxy.blue.ygrid.yahoo.com:4443/fs/'
-	operation = 'status'
-	ws_url = proxy_server + 'user/' + username + '?op=' + operation
-	curl_args = [ curl, '-c', 'proxycookie.txt', '--cacert', cert, '--negotiate', '-u', 'login:key', ws_url ]
-	process = subprocess.Popen(curl_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	#curl = subprocess.Popen(curl_args, shell=True)
-	out, err = process.communicate()
-	#curl.terminate();
-	print(out)
+        operation = 'status'
+        relative_path = ''
+        ws_url = proxy_server + 'user/' + username + '/' + relative_path + '?op=' + operation
+        curl_args = [ curl, '-c', 'proxycookie.txt', '--cacert', cert, '--negotiate', '-u', 'login:key', ws_url ]
+        process = subprocess.Popen(curl_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        #curl = subprocess.Popen(curl_args, shell=True)
+        out, err = process.communicate()
+        #curl.terminate();
+        print(out)
+        return HttpResponse("<html> <body><li>LIST "+relative_path+"</li> </body></html>" )
 
-	operation = 'mkdir'
-	newdir = 'newdir'
-        ws_url = proxy_server + 'user/' + username + '/newdir?op=' + operation
+def list(request):
+        first_load(request)
+        proxy_server = 'https://axoniteblue-nn1-pxy.blue.ygrid.yahoo.com:4443/fs/'
+        operation = 'status'
+        relative_path = ''
+        ws_url = proxy_server + 'user/' + username + '/' + relative_path + '?op=' + operation
+        curl_args = [ curl, '-c', 'proxycookie.txt', '--cacert', cert, '--negotiate', '-u', 'login:key', ws_url ]
+        process = subprocess.Popen(curl_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        #curl = subprocess.Popen(curl_args, shell=True)
+        out, err = process.communicate()
+        #curl.terminate();
+        print(out)
+        return HttpResponse("<html> <body><li>LIST "+relative_path+"</li> </body></html>" )
+
+def mkdir(request):
+        first_load(request)
+        proxy_server = 'https://axoniteblue-nn1-pxy.blue.ygrid.yahoo.com:4443/fs/'
+        operation = 'mkdir'
+        dirname = 'newdir'
+        ws_url = proxy_server + 'user/' + username + '/' + dirname + '?op=' + operation
         curl_args = [ curl, '-X', 'PUT', '-b', 'proxycookie.txt', '--cacert', cert, '--negotiate', '-u', 'login:key', ws_url ]
         process = subprocess.Popen(curl_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         #curl = subprocess.Popen(curl_args, shell=True)
         out, err = process.communicate()
         #curl.terminate();
         print(out)
+        return HttpResponse("<html> <body><li>MKDIR "+dirname+"</li> </body></html>" )
 
-	file = 'mona.txt'
-	operation = 'stream'
-        ws_url = proxy_server + 'user/' + username + '/' + file + '?op=' + operation
+def delete(request):
+        first_load(request)
+        proxy_server = 'https://axoniteblue-nn1-pxy.blue.ygrid.yahoo.com:4443/fs/'
+        filename = 'mona.txt'
+        operation = 'stream'
+        ws_url = proxy_server + 'user/' + username + '/' + filename + '?op=' + operation
         curl_args = [ curl, '-b', 'proxycookie.txt', '--cacert', cert, '--negotiate', '-u', 'login:key', ws_url, '--header', 'Range: bytes=0-1000' ]
         process = subprocess.Popen(curl_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         #curl = subprocess.Popen(curl_args, shell=True)
         out, err = process.communicate()
         #curl.terminate();
         print(out)
+        return HttpResponse("<html> <body><li>DELETED "+filename+"</li> </body></html>" )
 
-	return HttpResponse("<html> <body><li>"+out+"</li> </body></html>" )
-
-	dest = 'dest.file'
-	src = 'views.py.OLD'
-	operation = 'create'
+def upload(request):
+        first_load(request)
+        proxy_server = 'https://axoniteblue-nn1-pxy.blue.ygrid.yahoo.com:4443/fs/'
+        dest = 'dest.file'
+        src = 'views.py.OLD'
+        operation = 'create'
         ws_url = proxy_server + 'user/' + username + '/' + dest + '?op=' + operation + '&overwrite=true'
         curl_args = [ curl, '-X', 'PUT', '-b', 'proxycookie.txt', '--cacert', cert, '-T', src, '--negotiate', '-u', 'login:key', ws_url ]
         process = subprocess.Popen(curl_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -95,16 +127,53 @@ def hdfs_proxy(request):
         out, err = process.communicate()
         #curl.terminate();
         print(dest)
+        return HttpResponse("<html> <body><li>UPLOADED "+dest+"</li> </body></html>" )
 
-	return HttpResponse("<html> <body><li>"+dest+"</li> </body></html>" )
-	#curl = pycurl.Curl()
-	#curl.setopt(curl.VERBOSE, 1)
-	#curl.setopt(pycurl.URL, proxy)
-        #curl.setopt(pycurl.CAINFO, certdir)
-	#curl.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_GSSNEGOTIATE)
-	#curl.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_BASIC)
-	#curl.perform()
-	#curl.close()
+def move(request):
+        first_load(request)
+        proxy_server = 'https://axoniteblue-nn1-pxy.blue.ygrid.yahoo.com:4443/fs/'
+        operation = 'move'
+        src = 'srcdir'
+        dst = 'dstdir'
+        ws_url = proxy_server + 'user/' + username + '/' + src + '?op=' + operation + '&dest=' + dst
+        curl_args = [ curl, '-X', 'PUT', '-b', 'proxycookie.txt', '--cacert', cert, '--negotiate', '-u', 'login:key', ws_url ]
+        process = subprocess.Popen(curl_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        #curl = subprocess.Popen(curl_args, shell=True)
+        out, err = process.communicate()
+        #curl.terminate();
+        print(out)
+        return HttpResponse("<html> <body><li>MOVED "+src+" to "+dst+"</li> </body></html>" )
+
+def chmod(request):
+        first_load(request)
+        proxy_server = 'https://axoniteblue-nn1-pxy.blue.ygrid.yahoo.com:4443/fs/'
+        operation = 'chmod'
+        permission = '644'
+        relative_path = ''
+        ws_url = proxy_server + 'user/' + username + '/' + relative_path + '?op=' + operation + '&permission=' + permission + '&recursive=true'
+        curl_args = [ curl, '-X', 'PUT', '-b', 'proxycookie.txt', '--cacert', cert, '--negotiate', '-u', 'login:key', ws_url ]
+        process = subprocess.Popen(curl_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        #curl = subprocess.Popen(curl_args, shell=True)
+        out, err = process.communicate()
+        #curl.terminate();
+        print(out)
+        return HttpResponse("<html> <body><li>CHMOD "+dirname+"</li> </body></html>" )
+
+def chown(request):
+        first_load(request)
+        proxy_server = 'https://axoniteblue-nn1-pxy.blue.ygrid.yahoo.com:4443/fs/'
+        operation = 'chown'
+        owner = 'user'
+        relative_path = ''
+        ws_url = proxy_server + 'user/' + username + '/' + relative_path + '?op=' + operation + '&owner=' + owner + '&recursive=true'
+        curl_args = [ curl, '-X', 'PUT', '-b', 'proxycookie.txt', '--cacert', cert, '--negotiate', '-u', 'login:key', ws_url ]
+        process = subprocess.Popen(curl_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        #curl = subprocess.Popen(curl_args, shell=True)
+        out, err = process.communicate()
+        #curl.terminate();
+        print(out)
+        return HttpResponse("<html> <body><li>CHOWN "+relative_path+" to "+owner+"</li> </body></html>" )
+
 
 def browser_view(request):
 	template = loader.get_template('fileBrowser/browse.html')
