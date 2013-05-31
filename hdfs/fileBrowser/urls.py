6e9446db86ac6124517b463eab725b1f442a3007
Mona Chitnis <chitnis@crosslotoil-lm.corp.yahoo.com>
2013-05-31 03:17:02 -0700

    committing correct code

--- hdfs/fileBrowser/urls.py
+++ hdfs/fileBrowser/urls.py
@@ -4,10 +4,10 @@ from fileBrowser import views
 
 urlpatterns = patterns('',
     url(r'^browser$', views.browser_view, name='browser_view'),
+    url(r'^login/$', views.login_user, name='login_user'),
     url(r'^info$', views.dir_info, name='dir_info'),
     url(r'^gridListing$', views.grid_listing, name='grid_listing'),
     url(r'^api$', views.api_view, name='api_view'),
-    url(r'^hdfsBrowser$', views.first_load, name='first_load'),
     url(r'^list$', views.list, name='list'),
     url(r'^mkdir$', views.mkdir, name='mkdir'),
     url(r'^delete$', views.delete, name='delete'),
